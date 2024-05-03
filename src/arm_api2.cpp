eba63e56db35a9b6c1967bd08e7a841d5f27071e
Filip Zoric <fzoric8@gmail.com>
2024-05-03 18:34:15 +0200

    Added collision info

--- src/arm_api2.cpp
+++ src/arm_api2.cpp
@@ -3,6 +3,7 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
 
@@ -63,13 +64,47 @@ int main(int argc, char * argv[])
       };
 
   auto const target_pose = []{
-    geometry_msgs::msg::Pose msg; 
-    msg.orientation.w = 1.0; 
-    msg.position.x = 0.28; 
-    msg.position.y = -0.2; 
-    msg.position.z = 0.5; 
+    geometry_msgs::msg::Pose msg;
+    msg.orientation.y = 0.8; 
+    msg.orientation.w = 0.6; 
+    msg.position.x = 0.1; 
+    msg.position.y = 0.4; 
+    msg.position.z = 0.4; 
     return msg; 
   }(); 
+
+  auto const collision_object = [frame_id = move_group_interface.getPlanningFrame()] 
+  {
+    moveit_msgs::msg::CollisionObject collision_object; 
+    collision_object.header.frame_id = frame_id; 
+    collision_object.id = "box1"; 
+    shape_msgs::msg::SolidPrimitive primitive; 
+
+    // Define the size of the box in meters
+    primitive.type = primitive.BOX; 
+    primitive.dimensions.resize(3); 
+    primitive.dimensions[primitive.BOX_X] = 0.5; 
+    primitive.dimensions[primitive.BOX_Y] = 0.1; 
+    primitive.dimensions[primitive.BOX_Z] = 0.5; 
+
+    // Define the pose of the box (relative to the frame)
+    geometry_msgs::msg::Pose box_pose; 
+    box_pose.orientation.w = 1.0; 
+    box_pose.position.x = 0.2; 
+    box_pose.position.y = 0.2; 
+    box_pose.position.z = 0.25; 
+
+    collision_object.primitives.push_back(primitive); 
+    collision_object.primitive_poses.push_back(box_pose); 
+    collision_object.operation = collision_object.ADD; 
+
+    return collision_object; 
+  }(); 
+
+  // Add the collision object to the scene 
+  moveit::planning_interface::PlanningSceneInterface planning_scene_interface; 
+  planning_scene_interface.applyCollisionObject(collision_object); 
+                                    
 
   // setPoseTarget
   move_group_interface.setPoseTarget(target_pose); 
