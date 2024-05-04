aa63f1a9e041f113f1f823f4b0cec0fba8079987
Filip Zoric <fzoric8@gmail.com>
2024-05-05 00:56:08 +0200

    Tested subs/pubs and naming for m2_iface

--- src/moveit2_iface.cpp
+++ src/moveit2_iface.cpp
@@ -3,13 +3,15 @@
 m2Iface::m2Iface(): Node("moveit2_iface")
 {   
     ns_ = this->get_namespace(); 	
+    // TODO: Load yaml path from param
     config = init_config("/root/ws_moveit2/src/arm_api2/config/franka.yaml"); 
+    RCLCPP_INFO_STREAM(this->get_logger(), "Loaded config!");
     init_publishers(); 
     init_subscribers(); 
 }
 
 YAML::Node m2Iface::init_config(std::string yaml_path)
-{
+{   
     return YAML::LoadFile(yaml_path);
 }
 
@@ -17,15 +19,19 @@ void m2Iface::init_publishers()
 {   
     auto pose_state_name = config["topic"]["pub"]["current_pose"]["name"].as<std::string>(); 
     pose_state_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(ns_ + pose_state_name, 1); 
+    RCLCPP_INFO_STREAM(this->get_logger(), "Initialized publishers!");
 }
 
 void m2Iface::init_subscribers()
 {
     auto pose_cmd_name = config["topic"]["sub"]["cmd_pose"]["name"].as<std::string>(); 
-    pose_cmd_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(ns_ + pose_cmd_name, 1, std::bind(&m2Iface::pose_cmd_cb, this, _1)); 
+    pose_cmd_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(ns_ + pose_cmd_name, 1, std::bind(&m2Iface::pose_cmd_cb, this, _1));
+    RCLCPP_INFO_STREAM(this->get_logger(), "Initialized subscribers!"); 
 }
 
 void m2Iface::pose_cmd_cb(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
 {
-    std::cout << "Reciv msg" << msg << std::endl; 
+    geometry_msgs::msg::PoseStamped pose_cmd; 
+    pose_cmd.pose = msg->pose; 
+    std::cout << "Reciv msg" << pose_cmd << std::endl; 
 }
