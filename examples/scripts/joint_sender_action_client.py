20610fc5d6961bfbc1a732d42b86940e49569336
Edgar Welte <edgar.welte@kit.edu>
2025-01-29 16:20:14 +0000

    fix synchronous examples

--- examples/scripts/joint_sender_action_client.py
+++ examples/scripts/joint_sender_action_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import threading
 import rclpy
 from arm_api2_msgs.action import MoveJoint
 from sensor_msgs.msg import JointState
@@ -12,8 +13,12 @@ class JointSenderActionClient(Node):
     def __init__(self):
         super().__init__("joint_sender_action_client")
         self._action_client = ActionClient(self, MoveJoint, "arm/move_to_joint")
+        
+        # new thread to handle the actual task without blocking the main thread
+        self._execution_thread = threading.Thread(target=self._execute)
+        self._execution_thread.start()
 
-    def send_goal(self, goal: JointState):
+    def _send_goal(self, goal: JointState):
         goal_msg = MoveJoint.Goal()
         goal_msg.joint_state = goal
 
@@ -27,20 +32,23 @@ class JointSenderActionClient(Node):
         
         self.get_logger().info("Result: {0}".format(result))
         rclpy.shutdown()
+    
+    def _execute(self):
+        
+        names =["shoulder_pan_joint","shoulder_lift_joint","elbow_joint","wrist_1_joint","wrist_2_joint","wrist_3_joint"]
+        positions = [-4.0,-2.067,1.849,-1.428,-1.395,2.045]
+        goal = JointState()
+        goal.name = names
+        goal.position = positions
+
+        self._send_goal(goal)
         
 
 def main(args=None):
     rclpy.init(args=args)
 
     action_client = JointSenderActionClient()
-    names =["shoulder_pan_joint","shoulder_lift_joint","elbow_joint","wrist_1_joint","wrist_2_joint","wrist_3_joint"]
-    positions = [-4.0,-2.067,1.849,-1.428,-1.395,2.045]
-    goal = JointState()
-    goal.name = names
-    goal.position = positions
-
-    action_client.send_goal(goal)
-
+    
     rclpy.spin(action_client)
 
 
